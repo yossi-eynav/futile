22d0bbd1404c9da30a17ab4f090f79807c9e69f0
Omri Lotan <omri@hamadgera.com>
2017-03-05 08:42:39 +0200

    Rename commodity to futile

--- playground/example.js
+++ playground/example.js
@@ -1,10 +1,10 @@
-const commodity = require('@fiverr/commodity');
-console.log(commodity.shuffle([1, 2, 3]));
-console.log(commodity.interpolate('Hello, ${name}', {name: 'Martin'}));
+const futile = require('@fiverr/futile');
+console.log(futile.shuffle([1, 2, 3]));
+console.log(futile.interpolate('Hello, ${name}', {name: 'Martin'})); // eslint-disable-line no-template-curly-in-string
 
 
-const shuffle = require('@fiverr/commodity/lib/shuffle');
+const shuffle = require('@fiverr/futile/lib/shuffle');
 console.log(shuffle([1, 2, 3]));
 
-const interpolate = require('@fiverr/commodity/lib/interpolate');
-console.log(interpolate('Hello, ${name}', {name: 'Martin'}));
+const interpolate = require('@fiverr/futile/lib/interpolate');
+console.log(interpolate('Hello, ${name}', {name: 'Martin'})); // eslint-disable-line no-template-curly-in-string
